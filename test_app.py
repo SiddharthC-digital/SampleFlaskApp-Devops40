f30ef1cf3d229cb1bacf863a4ea46af598ef1d65
Sudhanshu Pandey <57751726+sudhanshuvlog@users.noreply.github.com>
2025-12-27 13:06:57 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -31,8 +31,6 @@ def test_test1(client):
     assert response.status_code == 200
     expected_text = 'test'
     assert expected_text.encode() in response.data
-
-
 def test_test2(client):
     response = client.get('/test3')
     assert response.status_code == 200
